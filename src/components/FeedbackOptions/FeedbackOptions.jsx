6f63a4a55e33bef778fe6dc3b24f12d12482236c
uflakey <134938478+uflakey@users.noreply.github.com>
2023-12-24 13:55:49 +0200

    Update FeedbackOptions.jsx

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,11 +1,18 @@
-const FeedbackOptions = ({ options, onLeaveFeedback }) => (
-  <div>
-    {options.map(option => (
-      <button key={option} onClick={() => onLeaveFeedback(option)}>
+import React from 'react';
+
+const FeedbackOptions = ({ options = [], onLeaveFeedback }) => {
+  return options.map(option => (
+    <>
+      <button
+        key={option}
+        type="button"
+        name={option}
+        onClick={onLeaveFeedback}
+        
+      >
         {option}
       </button>
-    ))}
-  </div>
-);
-
+    </>
+  ));
+};
 export default FeedbackOptions;
